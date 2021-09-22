bdfc4af2ea9e4e53e308bc5661a627e8ed1cc3cb
ludovicjj <jahanlud@gmail.com>
2021-09-22 14:25:02 +0200

    Create method ::on for setup event and method ::emit for run event

--- src/Observer/Emitter.php
+++ src/Observer/Emitter.php
@@ -7,6 +7,11 @@ class Emitter
     private static $_instance;
 
     /**
+     * @var callable[][]
+     */
+    private $listeners = [];
+
+    /**
      * Return same instance of Emitter (singleton)
      *
      * @return static
@@ -18,5 +23,31 @@ class Emitter
         }
 
         return self::$_instance;
+    }
+
+    /**
+     * Setup event
+     *
+     * @param string $event Event name
+     * @param callable $callable Callable to run for this event
+     */
+    public function on(string $event, callable $callable)
+    {
+        $this->listeners[$event][] = $callable;
+    }
+
+    /**
+     * Send event
+     *
+     * @param string $event Event name
+     * @param mixed ...$args Parameters for callable
+     */
+    public function emit(string $event, ...$args)
+    {
+        if (array_key_exists($event, $this->listeners)) {
+            foreach ($this->listeners[$event] as $listener) {
+                call_user_func_array($listener, $args);
+            }
+        }
     }
 }
