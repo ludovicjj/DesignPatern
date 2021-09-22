4dc78e0ba6e37d8ab15600af01f0bad4208666c6
ludovicjj <jahanlud@gmail.com>
2021-09-22 14:26:05 +0200

    Test: emit event trigger callable

--- tests/EmitterTest.php
+++ tests/EmitterTest.php
@@ -4,6 +4,7 @@ namespace Test;
 
 use App\Observer\Emitter;
 use PHPUnit\Framework\TestCase;
+use stdClass;
 
 class EmitterTest extends TestCase
 {
@@ -13,5 +14,30 @@ class EmitterTest extends TestCase
         $emitter2 = Emitter::getInstance();
 
         $this->assertSame($emitter1, $emitter2);
+    }
+
+    public function testEmitEventTriggerCallable(): void
+    {
+        $emitter = Emitter::getInstance();
+        $listener = $this->getMockBuilder(stdClass::class)->addMethods(["onSend"])->getMock();
+
+        $emitter->on("Test.event", [$listener, "onSend"]);
+
+        $listener->expects($this->once())->method("onSend")->with($this->stringContains('John'));
+
+        $emitter->emit("Test.event", "John");
+    }
+
+    public function testEmitEventTriggerCallableManyTime(): void
+    {
+        $emitter = Emitter::getInstance();
+        $listener = $this->getMockBuilder(stdClass::class)->addMethods(["onSend"])->getMock();
+
+        $emitter->on("Test.event", [$listener, "onSend"]);
+
+        $listener->expects($this->exactly(2))->method("onSend")->with($this->stringContains('John'));
+
+        $emitter->emit("Test.event", "John");
+        $emitter->emit("Test.event", "John");
     }
 }
