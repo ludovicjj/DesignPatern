95750afcf88cdc0300c449e18b985b10d51453a1
ludovicjj <jahanlud@gmail.com>
2021-09-22 13:53:20 +0200

    Singleton

--- src/Observer/Emitter.php
+++ src/Observer/Emitter.php
@@ -4,5 +4,19 @@ namespace App\Observer;
 
 class Emitter
 {
+    private static $_instance;
 
+    /**
+     * Return same instance of Emitter (singleton)
+     *
+     * @return static
+     */
+    public static function getInstance(): self
+    {
+        if (!self::$_instance) {
+            self::$_instance = new self();
+        }
+
+        return self::$_instance;
+    }
 }
