da016d6a7dadb6e0b17b5e1f72fb69618e67ea58
ludovicjj <jahanlud@gmail.com>
2021-09-23 11:13:44 +0200

    Use reflection to reset property _instance before each tests

--- tests/EmitterTest.php
+++ tests/EmitterTest.php
@@ -1,14 +1,31 @@
 <?php
 
-namespace Test;
+namespace Tests;
 
 use App\Observer\Emitter;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 use stdClass;
+use ReflectionClass;
 
 class EmitterTest extends TestCase
 {
+    /**
+     * @var Emitter $emitter
+     */
+    private $emitter;
+
+    protected function setUp(): void
+    {
+        $reflexionClass = new ReflectionClass(Emitter::class);
+        $property = $reflexionClass->getProperty("_instance");
+        $property->setAccessible(true);
+        $property->setValue(null, null);
+        $property->setAccessible(false);
+
+        $this->emitter = Emitter::getInstance();
+    }
+
     public function testEmitterInstanceIsSingleton(): void
     {
         $emitter1 = Emitter::getInstance();
@@ -19,27 +36,25 @@ class EmitterTest extends TestCase
 
     public function testEmitEventTriggerCallable(): void
     {
-        $emitter = Emitter::getInstance();
         $listener = $this->mockListener();
 
-        $emitter->on("Test.event", [$listener, "onSend"]);
+        $this->emitter->on("Test.event", [$listener, "onSend"]);
 
         $listener->expects($this->once())->method("onSend")->with($this->stringContains('John'));
 
-        $emitter->emit("Test.event", "John");
+        $this->emitter->emit("Test.event", "John");
     }
 
     public function testEmitEventTriggerCallableManyTime(): void
     {
-        $emitter = Emitter::getInstance();
         $listener = $this->mockListener();
 
-        $emitter->on("Test.event", [$listener, "onSend"]);
+        $this->emitter->on("Test.event", [$listener, "onSend"]);
 
         $listener->expects($this->exactly(2))->method("onSend")->with($this->stringContains('John'));
 
-        $emitter->emit("Test.event", "John");
-        $emitter->emit("Test.event", "John");
+        $this->emitter->emit("Test.event", "John");
+        $this->emitter->emit("Test.event", "John");
     }
 
     private function mockListener(): MockObject
