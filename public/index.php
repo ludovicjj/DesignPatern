740d55769673ddf16a598255a02450bbfece50f7
ludovicjj <jahanlud@gmail.com>
2021-09-27 14:07:35 +0200

    Clear index.php

--- public/index.php
+++ public/index.php
@@ -1,16 +1,3 @@
 <?php
 
 require "../vendor/autoload.php";
-
-use App\Middleware\HandlerMiddleware;
-use App\Middleware\PoweredByAMiddleware;
-use App\Middleware\PoweredByBMiddleware;
-use GuzzleHttp\Psr7\ServerRequest;
-use function Http\Response\send;
-
-$request = ServerRequest::fromGlobals();
-
-$dispatcher = new HandlerMiddleware();
-$dispatcher->pipe(new PoweredByAMiddleware());
-$dispatcher->pipe(new PoweredByBMiddleware());
-send($dispatcher->handle($request));
