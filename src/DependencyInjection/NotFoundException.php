1e4736a0958232295c1f18d64b40f10ad119f2c3
ludovicjj <jahanlud@gmail.com>
2021-09-28 11:20:07 +0200

    Create custom exception implement NotFoundExceptionInterface (psr11)

---
+++ src/DependencyInjection/NotFoundException.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace App\DependencyInjection;
+
+use Exception;
+use Psr\Container\NotFoundExceptionInterface;
+
+class NotFoundException extends Exception implements NotFoundExceptionInterface
+{
+
+}
