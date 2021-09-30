8ac1929bc6f5afa9ea1db66e27a5f57f84def3f9
ludovicjj <jahanlud@gmail.com>
2021-09-30 15:32:57 +0200

    Update ContainerInterface

--- src/DependencyInjection/ContainerInterface.php
+++ src/DependencyInjection/ContainerInterface.php
@@ -7,5 +7,45 @@ use Psr\Container\ContainerInterface as PsrContainerInterface;
 
 interface ContainerInterface extends PsrContainerInterface
 {
+    /**
+     * Return definition (singleton).
+     *
+     * @param string $id
+     * @return Definition
+     */
+    public function getDefinition(string $id): Definition;
 
+    /**
+     * Create definition for asked class.
+     * Create definition foreach dependencies of asked class.
+     *
+     * @param string $id
+     */
+    public function resolveDefinition(string $id): void;
+
+    /**
+     * Register an interface bind to a class.
+     *
+     * @param string $alias
+     * @param string $target
+     * @return mixed
+     */
+    public function addAlias(string $alias, string $target): self;
+
+    /**
+     * Register a parameter with his value.
+     *
+     * @param string $name
+     * @param mixed $value
+     * @return $this
+     */
+    public function addParameter(string $name, $value): self;
+
+    /**
+     * Get parameter value.
+     *
+     * @param string $name
+     * @return mixed
+     */
+    public function getParameter(string $name);
 }
